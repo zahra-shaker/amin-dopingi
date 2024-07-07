7a7ed02eb601fabcf6bf81dc61225b49e4a79b80
zahra-shaker <zshaker2005@gmail.com>
2024-07-07 16:58:55 +0330

    add file game.h

---
+++ Game.h
@@ -0,0 +1,19 @@
+//
+// Created by AC30 on 7/7/2024.
+//
+
+#ifndef AMIN_DOPINGI_GAME_H
+#define AMIN_DOPINGI_GAME_H
+
+#include <QGraphicsView>
+
+
+class Game : public QGraphicsView {
+
+public:
+    Game() ;
+
+};
+
+
+#endif //AMIN_DOPINGI_GAME_H
